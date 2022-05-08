75f32d5f294a21c3e2034edd162ac0b80aec4622
AnandaIlyasa <anandailyasa23244@student.uns.ac.id>
2022-05-08 10:16:42 +0800

    setup eureka discovery server project

---
+++ src/test/java/com/ilyas/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilyas.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
